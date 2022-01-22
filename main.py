c57c73c9a98c123b87238b297b8afcda5b353f5f
ben-huangus <uclaidiot@gmail.com>
2022-01-22 10:22:54 -0600

    Modified on 01/22/2022

--- main.py
+++ main.py
@@ -1,77 +1,126 @@
 
+import psycopg2
+import pandas as pd
+import json
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+import ast
+from apache_beam.io import WriteToText, fileio
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions
 from apache_beam.io.gcp.internal.clients import bigquery
+from datetime import datetime
+from google.cloud import pubsub_v1
+
+
 
 
 if __name__ == '__main__':
+    # table_schema = {
+    #     'fields': [
+    #         {'name': 'title', 'type': 'STRING', 'mode': 'Required'},
+    #         {'name': 'release_year', 'type': 'INTEGER', 'mode': 'Nullable'},
+    #         {'name': 'rating', 'type': 'STRING', 'mode': 'Nullable'},
+    #         {'name': 'special_features', 'type': 'STRING', 'mode': 'Nullable'},
+    #         {'name': 'category', 'type': 'STRING', 'mode': 'Nullable'}
+    #     ]}
+    #
+    # table_spec1 = bigquery.TableReference(
+    #     projectId = 'york-cdf-start',
+    #     datasetId='final-ben-huang',
+    #     tableId='special_films')
 
-    #table_schema for BigQuery
-    cnt_schema = {
-        'fields': [
-            {'name': 'cust_tier_code', 'type': 'STRING', 'mode': 'Required'},
-            {'name': 'sku', 'type': 'INTEGER', 'mode': 'Required'},
-            {'name': 'total_no_of_product_views', 'type': 'INTEGER', 'mode': 'Required'}
-        ]
-    }
+    # read_options = PipelineOptions(
+    #     #runner="DataflowRunner",
+    #     project="york-cdf-start",
+    #     region="us-central1",
+    #     custom_gcs_temp_location="gs://york_temp_files/tmp6")
+    #     #job_name="ben-huang-final-job")
 
-    sum_schema = {
-        'fields': [
-            {'name': 'cust_tier_code', 'type': 'STRING', 'mode': 'Required'},
-            {'name': 'sku', 'type': 'INTEGER', 'mode': 'Required'},
-            {'name': 'total_sales_amount', 'type': 'FLOAT', 'mode': 'Required'}
-        ]
-    }
+      ## Read from pgAdmin on aws
+    conn = psycopg2.connect(database="benhuangus1", user="dbmasteruser",
+                            password="...",
+                            host="ls-41d379b19b475ed294babb170cfa0f93b3011e47.cq2f1e9koedo.us-east-2.rds.amazonaws.com")
+    print("Opened database successfully");
+    #cur = conn.cursor()
 
-    table_spec1 = bigquery.TableReference(
-        projectId="york-cdf-start",
-        datasetId="final_ben_huang",
-        tableId="cust_tier_code-sku-total_no_of_product_views")
+    sql = """select f.title, 
+                    f.release_year,
+        	        f.rating,
+        	        f.special_features,
+        	        c.name as category
+                    from public.film f inner join public.film_category fc
+                         on f.film_id = fc.film_id inner join public.category c
+        	             on fc.category_id = c.category_id
+                    where f.title in (
+        	                        select f.title from public.film f inner join public.film_actor fa
+        	                        on f.film_id = fa.film_id inner join public.actor ac
+        	                        on fa.actor_id = ac.actor_id
+        	                        where ac.first_name LIKE 'B%')
+                    limit 1000 """
 
-    table_spec2 = bigquery.TableReference(
-        projectId="york-cdf-start",
-        datasetId="final_ben_huang",
-        tableId="cust_tier_code-sku-total_sales_amount")
+    df = pd.read_sql(sql, conn)
+    df.to_csv('gs://york-project-bucket/films2')
 
-    read_options = PipelineOptions(
-        runner="DataflowRunner",
-        project="york-cdf-start",
-        region="us-central1",
-        temp_location="gs://york_temp_files/tmp",
-        job_name="ben-huang-final-job")
+    ## convert to Json if needed
+    # output = df.to_json(orient='records')[1:-1].replace('},{', '} {')
 
-    with beam.Pipeline(options=read_options) as p:
-        product_views = p | 'Read-cnt' >> beam.io.ReadFromBigQuery(query="""SELECT cast(c.CUST_TIER_CODE as string) as cust_tier_code, cast(p.SKU as integer) as sku, count(p.EVENT_TM) as total_no_of_product_views                                                                   
-                                                                            FROM `york-cdf-start.final_input_data.product_views` p inner join 
-                                                                                 `york-cdf-start.final_input_data.customers` c 
-                                                                                  on c.CUSTOMER_ID = p.CUSTOMER_ID                                                                            
-                                                                            group by cast(c.CUST_TIER_CODE as string),cast(p.SKU as integer) """, project = "york-cdf-start", use_standard_sql=True)
+    from google.cloud import bigquery
+    client = bigquery.Client()
 
-        orders_sum = p | 'Read-sum' >> beam.io.ReadFromBigQuery(query="""SELECT cast(c.CUST_TIER_CODE as string) as cust_tier_code, o.SKU as sku, sum(o.ORDER_AMT) as total_sales_amount                                                                   
-                                                                         FROM `york-cdf-start.final_input_data.orders` o inner join 
-                                                                              `york-cdf-start.final_input_data.customers` c 
-                                                                               on c.CUSTOMER_ID = o.CUSTOMER_ID                                                                            
-                                                                         group by cast(c.CUST_TIER_CODE as string),o.SKU  """, project="york-cdf-start", use_standard_sql=True)
+    # TODO(developer): Set table_id to the ID of the table to create.
+
+    table_id = "york-cdf-start.final_ben_huang.aws-table-load_special-films"
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("id", "INTEGER"),
+            bigquery.SchemaField("title", "STRING"),
+            bigquery.SchemaField("release_year", "INTEGER"),
+            bigquery.SchemaField("rating", "STRING"),
+            bigquery.SchemaField("special_features", "STRING"),
+            bigquery.SchemaField("category", "STRING"),
+        ],
+        skip_leading_rows=1,
+        # The source format defaults to CSV...
+        source_format=bigquery.SourceFormat.CSV,
+    )
+    uri = "https://storage.cloud.google.com/york-project-bucket/films2"
+
+    load_job = client.load_table_from_uri(
+        uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()
+
+    destination_table = client.get_table(table_id)  # Make an API request.
+    print("Loaded {} rows..complete".format(destination_table.num_rows))
+
+    #options = beam.options.pipeline_options.PipelineOptions(streaming=True)
+    # with beam.Pipeline() as p:
+    #
+    #     data = (p | beam.io.ReadFromText(
+    #             "gs://york-project-bucket/temp9")
+    #               | beam.Map(print)
+    #             )
+
+        # data | "Write-Table" >> beam.io.WriteToBigQuery(
+        #     table_spec1,
+        #     schema=table_schema,
+        #     custom_gcs_temp_location = "gs://york-project-bucket/temp6",
+        #     write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+        #     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
 
 
 
-        product_views | "Write to bigquery1" >> beam.io.WriteToBigQuery(
-            table_spec1,
-            schema=cnt_schema,
-            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
-        )
 
-        orders_sum | "Write to bigquery2" >> beam.io.WriteToBigQuery(
-            table_spec2,
-            schema=sum_schema,
-            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
-        )
+
+    pass
 
 
 
-    
-    # Jenkins - testing
-    print("Hello Jenkins!")
+
+
+
+
 
 
 
